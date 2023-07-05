7de5e5a49d515564ca47088f4e32af94ac126203
daixin315 <69029489+daixin315@users.noreply.github.com>
2023-07-05 14:10:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,8 @@
 # Add a feed source
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> /root/lede/feeds.conf.default
-echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> /root/lede/feeds.conf.default
+# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> /root/lede/feeds.conf.default
+# echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> /root/lede/feeds.conf.default
 
-#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' >> feeds.conf.default
-#echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' >> feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages' >> feeds.conf.default
+echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' >> feeds.conf.default
